30c9b875048a772636fbaff667c48a2389463496
Chris Underwood <cunderwoodmn@gmail.com>
2018-03-07 23:42:35 -0800

    displayed records to the web page

--- src/App.js
+++ src/App.js
@@ -25,16 +25,6 @@ componentDidMount() {
   });
 }
 
-componentDidMount() {/*
-   fetch("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC")
-     .then(response=> response.json())
-     .then(responseData => {
-       this.setState({gifs: responseData.data});
-     })
-     .catch(error => {
-       console.log('Error fetching and parsing data', error);
-     });
- */}
 
 
   render() {
@@ -48,7 +38,7 @@ componentDidMount() {/*
           </div>
         </div>
         <div className="main-content">
-          <GifList />
+          <GifList data={this.state.gifs}/>
         </div>
       </div>
     );
